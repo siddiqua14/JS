0930ce39eae67eb2334685e227326ded2003f0d8
Siddiqua Mum <88455531+siddiqua14@users.noreply.github.com>
2024-04-24 18:05:57 +0000

    datatype

---
+++ 01/datatype.js
@@ -0,0 +1,31 @@
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "mumu"
+let age = 23
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof state);
