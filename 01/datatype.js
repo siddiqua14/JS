42d3f97004493aa35a2c220d2dfb18747e591161
Siddiqua Mum <88455531+siddiqua14@users.noreply.github.com>
2024-04-24 18:27:45 +0000

    conversion

--- 01/datatype.js
+++ 01/datatype.js
@@ -12,7 +12,7 @@ console.log("Hitesh")
 
 let name = "mumu"
 let age = 23
-let isLoggedIn = true
+//let isLoggedIn = true
 let state;
 
 // number => 2 to power 53
@@ -29,3 +29,69 @@ console.log(typeof age);
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
 console.log(typeof state);
+
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "33"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "mumu" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "mumu"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
