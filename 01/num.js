3077f9d3c8a9cd2c3b246330c9359f40c79dd1d8
Siddiqua Mumu <88455531+siddiqua14@users.noreply.github.com>
2024-05-28 14:21:42 +0000

    num

--- 01/num.js
+++ 01/num.js
@@ -1,19 +1,19 @@
 
-const score = 400
+const score = 400  
 // console.log(score);
 
-const balance = new Number(100)
+const balance = new Number(100)  //exclusively
 // console.log(balance);
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));  //more applicable for ecom
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));
+ console.log(otherNumber.toPrecision(5));
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+ console.log(hundreds.toLocaleString('en-IN')); //comma used
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
@@ -25,11 +25,11 @@ const hundreds = 1000000
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+//console.log(Math.random());
+//console.log((Math.random()*10) + 1);
+//console.log(Math.floor(Math.random()*10) + 1);
 
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+//onsole.log(Math.floor(Math.random() * (max - min + 1)) + min)
