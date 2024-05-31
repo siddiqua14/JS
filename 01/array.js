e0ba614243eda11e90063f9854241b7628747716
Siddiqua Mumu <88455531+siddiqua14@users.noreply.github.com>
2024-05-31 15:42:26 +0000

    array1

--- 01/array.js
+++ 01/array.js
@@ -1,46 +1,47 @@
 // array
 
 const myArr = [0, 1, 2, 3, 4, 5]
-const myHeors = ["shaktiman", "naagraj"]
+const myHeors = ["sumaiya", "mumu"]
 
 const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr[1]);
 
 // Array methods
 
-// myArr.push(6)
-// myArr.push(7)
+ //myArr.push(6)
+ //myArr.push(7)
+ //console.log(myArr)
 // myArr.pop()
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(9) //starting a new value add 
+// myArr.shift()    // starting er value remove
 
-// console.log(myArr.includes(9));
+// console.log(myArr.includes(9)); // 9 inclue ache kina
 // console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
+// const newArr = myArr.join()  // array er elemnt ke string convrt kore
 
 // console.log(myArr);
 // console.log( newArr);
 
 
-// slice, splice
+// slice, splice   // 
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(1, 3) // return a section of array 
 
 console.log(myn1);
 console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1, 3)  // original array ke manupulate kore 
 console.log("C ", myArr);
 console.log(myn2);
 
 
 
-// ================================//
+// ================================ //
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
