9f73c8e4d0fa463f0fb0359b1a2417361c7b3b55
Siddiqua Mumu <88455531+siddiqua14@users.noreply.github.com>
2024-06-05 10:16:59 +0000

    obj

--- 01/object.js
+++ 01/object.js
@@ -31,8 +31,92 @@ JsUser.greeting = function(){
     console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+    console.log(`Hello JS user, ${this.name}`); // string inter polation // this--> same object ke refer kore
 }
 
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
+
+
+// -------------------------------//
+// const tinderUser = new Object() // singleton
+const tinderUser = {}              // nonsingleton
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "siddiqua",
+            lastname: "mumu"
+        }
+    }
+}
+
+// console.log(regularUser.fullname?.userfullname.firstname);  // ?---> jodi present na thake fullname
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in git",
+    price: "999",
+    courseInstructor: "abc"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "abc",
+//     "coursename": "js in git",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
